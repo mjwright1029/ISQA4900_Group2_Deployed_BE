1585ee3210f297e8b9ce308d54f711628ff360b5
mjwright1029 <151294605+mjwright1029@users.noreply.github.com>
2025-11-23 19:23:31 -0600

    error fix

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -78,7 +78,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+#STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
